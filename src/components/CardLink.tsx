76e1f862b2c172fd32624fd3722363d1bc17cee5
Plan C <hubenchang0515@outlook.com>
2026-04-17 00:11:47 +0800

    样式调整

--- src/components/CardLink.tsx
+++ src/components/CardLink.tsx
@@ -9,7 +9,7 @@ export default function CardLink(props:CardLinkProps) {
             href={props.path} 
             title={props.text}
             style={{"background":'url(/pdf.png)', "background-size": 'cover'}}
-            class="w-full h-full outline-none select-none flex rounded-lg hover:scale-105 text-indigo-950 dark:text-sky-600 hover:text-emerald-500">
+            class="w-full h-full outline-none select-none flex rounded-lg hover:scale-105 text-indigo-950 dark:text-sky-600 hover:text-emerald-500 break-all">
             <span class="w-full flex justify-center font-bold pt-12">
                 <span class="text-shadow-lg">《{props.text}》</span>
             </span>
